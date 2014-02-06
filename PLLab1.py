0301e4811f8b793012c08df6442e528565923317
Olexandr <kolexandr.13@gmail.com>
2014-02-06 04:26:14 +0200

    some changes in inverse_case function

--- PLLab1.py
+++ PLLab1.py
@@ -26,7 +26,8 @@ print(to_lower_case("qwERTy"))
 
 
 #change register of letters to inverse
-inverse_case = lambda m: reduce(lambda x, y: x + y, map(lambda c: c.upper() if c != c.upper() else c.lower(), m))
+# inverse_case = lambda m: reduce(lambda x, y: x + y, map(lambda c: c.upper() if c != c.upper() else c.lower(), m))
+inverse_case = lambda m: reduce(lambda x, y: x + y, map(lambda c: c.swapcase(), m))
 print(inverse_case("qwERTy"))
 
 
